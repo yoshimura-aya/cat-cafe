a8091ceddcb23a1abcb40b6d4afe131e28d2a1f7
yoshimura-aya <aya.yoshimura@systemi.co.jp>
2022-11-17 23:20:55 +0900

    ハンズオン(HttpServer)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
